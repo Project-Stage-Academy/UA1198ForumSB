aa07d5475bc2ede52111b5288e0f24c73e5f10a8
Iryna Lytvyn <i.litvin27@gmail.com>
2024-07-04 17:22:19 +0300

    created user model

--- forum/users/models.py
+++ forum/users/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError("The Email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser):
+    class Role(models.IntegerChoices):
+        INVESTOR = 1
+        STARTUP = 2
+
+    user_id = models.AutoField(primary_key=True)
+    first_name = models.CharField()
+    last_name = models.CharField()
+    email = models.EmailField(unique=True)
+    user_phone = models.CharField()
+    description = models.CharField()
+    role = models.IntegerField(choices=Role.choices)
+    created_at = models.DateTimeField(default=timezone.now)
+    last_updated = models.DateTimeField(default=timezone.now)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
