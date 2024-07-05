ef5578e8f99ccdedde2bf79717bc6d4e92cb8ec5
Ivblackking <ivan.lysenko305@gmail.com>
2024-07-05 23:33:54 +0300

    debug and secret_key from .env file

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dp&#n$b4aqy*3cr1fw0s=#_w(7mijz^eo363%g09y5^x8h%q#b'
+SECRET_KEY = environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = []
 
