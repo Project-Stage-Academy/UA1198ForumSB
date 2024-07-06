4e0f2c6aa955b6a4eeba0cf1036d1c9b6fa9b325
m-o-d-e-r <moder.mode.on@gmail.com>
2024-07-06 17:23:23 +0300

    create simple tests

--- forum/projects/tests.py
+++ forum/projects/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+class ProjectTestCase(APITestCase):
+    def test_projects_industries(self):
+        response = self.client.get("/startups/projects/industries/")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- forum/startups/tests.py
+++ forum/startups/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class StartupsTestCase(APITestCase):
+    def test_industries_list(self):
+        response = self.client.get("/startups/startup_sizes/")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
