52d7a52a1b0473bc49c010e3d59172e339c7609b
IrinLytvyn <150450436+IrinLytvyn@users.noreply.github.com>
2024-07-08 20:28:08 +0300

    created user model (#34)
    * updated models
    
    * fixed bugs in user model
    
    * changed AUTH_USER_MODEL
    
    * fix supersuer creation
    
    * Changes to models
    
    * added project status model
    
    * changed IntegerField to PositiveIntegerField

--- forum/projects/models.py
+++ forum/projects/models.py
@@ -4,49 +4,54 @@ from investors.models import Investor
 from django.utils import timezone
 
 
-class Project(models.Model):
-    class ProjectStatus(models.TextChoices):
-        NEW = 'NEW'
-        ACTIVE = 'ACTIVE'
-        ON_HOLD = 'ON_HOLD'
-        COMPLETED = 'COMPLETED'
-        CANCELLED = 'CANCELLED'
-        PENDING = 'PENDING'
-        APPROVED = 'APPROVED'
+class ProjectStatus(models.Model):
+    status_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=75)
+    description = models.TextField(blank=True, null=True)
 
+    class Meta:
+        db_table = 'project_status'
+        verbose_name = 'ProjectStatus'
+        verbose_name_plural = 'ProjectStatuses'
+
+
+class Project(models.Model):
     project_id = models.AutoField(primary_key=True)
-    startup_id = models.ForeignKey(Startup, on_delete=models.CASCADE)
-    status_id = models.CharField(max_length=10, choices=ProjectStatus.choices, default=ProjectStatus.NEW)
+    startup = models.OneToOneField(Startup, on_delete=models.CASCADE, related_name="project")
+    status = models.ForeignKey(ProjectStatus, on_delete=models.CASCADE, related_name="projects")
     title = models.CharField(max_length=200)
     business_plan = models.TextField(blank=True, null=True)
-    duration = models.IntegerField(blank=True, null=True, help_text="Duration of the project in months")
+    duration = models.PositiveIntegerField(blank=True, null=True, help_text="Duration of the project in months")
     last_updated = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
     description = models.TextField(blank=True, null=True)
-    budget = models.IntegerField(blank=True, null=True)
+    budget = models.PositiveIntegerField(blank=True, null=True)
 
     class Meta:
         db_table = 'project'
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
 
 
 class ProjectSubscription(models.Model):
-    project_id = models.ForeignKey(Project, on_delete=models.CASCADE)
-    investor_id = models.ForeignKey(Investor, on_delete=models.CASCADE)
-    part = models.DecimalField(
-        max_digits=5,  # Maximum number of digits (including decimals)
-        decimal_places=2,  # Number of decimal places
-        help_text="Investor's share in the project as a percentage"
-    )
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="project_subscriptions")
+    investor = models.ForeignKey(Investor, on_delete=models.CASCADE, related_name="project_subscriptions")
+    part = models.PositiveIntegerField()
 
     class Meta:
         db_table = 'project_subscription'
+        verbose_name = 'ProjectSubscription'
+        verbose_name_plural = 'ProjectSubscriptions'
 
 
 class Industry(models.Model):
     industry_id = models.AutoField(primary_key=True)
-    projects = models.ManyToManyField(Project, related_name='industry')
+    projects = models.ManyToManyField(Project, related_name='industries')
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
 
     class Meta:
         db_table = 'industry'
+        verbose_name = 'Industry'
+        verbose_name_plural = 'Industries'
+
