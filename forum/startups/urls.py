77f19379af8499ad66f8e5ea570edb3a6080a3a5
Ivblackking <ivan.lysenko305@gmail.com>
2024-07-08 23:14:54 +0300

    added StartupView to urls

--- forum/startups/urls.py
+++ forum/startups/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import StartupSizeViewSet
+from .views import StartupSizeViewSet, StartupViewSet
 
 router = DefaultRouter()
 router.register('startup_sizes', StartupSizeViewSet)
+router.register('', StartupViewSet)
 
 
 urlpatterns = [
