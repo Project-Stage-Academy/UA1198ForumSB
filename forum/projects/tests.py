6bee00aa7a65946eda845889eac93316dd51ac7c
m-o-d-e-r <moder.mode.on@gmail.com>
2024-07-08 23:37:26 +0300

    update tests  & urls

--- forum/projects/tests.py
+++ forum/projects/tests.py
@@ -1,9 +1,26 @@
-from rest_framework.test import APITestCase
+from rest_framework.reverse import reverse
 from rest_framework import status
+from forum.tests_setup import TestUserSetupMixin
+
+from projects.models import Industry
 
 
-class ProjectTestCase(APITestCase):
+class IndustryModelTestCase(TestUserSetupMixin):
+    CREATE_INDUSTRY_COUNT: int = 3
+
+    def setUp(self) -> None:
+        self.setup_test_user()
+
+        # create industries
+        for i in range(self.CREATE_INDUSTRY_COUNT):
+            tmp_industry = Industry.objects.create(
+                name=f"industry {i}",
+                description=f"test description {i}"
+            )
+            Industry.objects.get(industry_id=tmp_industry.industry_id)
+
     def test_projects_industries(self):
-        response = self.client.get("/startups/projects/industries/")
+        response = self.client.get(reverse('industries-list'))
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.json()), self.CREATE_INDUSTRY_COUNT)
--- forum/startups/urls.py
+++ forum/startups/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import StartupSizeViewSet
 
 router = DefaultRouter()
-router.register('startup_sizes', StartupSizeViewSet)
+router.register('startup_sizes', StartupSizeViewSet, 'startup_sizes')
 
 
 urlpatterns = [
