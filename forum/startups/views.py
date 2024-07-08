19d4f34a3fb59411ac79971af786a18325d201a8
Ivblackking <ivan.lysenko305@gmail.com>
2024-07-08 23:12:30 +0300

    added StartupViewSet with list and retrieve methods

--- forum/startups/views.py
+++ forum/startups/views.py
@@ -1,10 +1,11 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.mixins import ListModelMixin
-from rest_framework.viewsets import GenericViewSet
+from rest_framework.viewsets import GenericViewSet, ViewSet
 from rest_framework.response import Response
 from drf_yasg.utils import swagger_auto_schema
 
-from .models import StartupSize
-from .serializers import StartupSizeSerializer
+from .models import StartupSize, Startup
+from .serializers import StartupSizeSerializer, StartupSerializer
 
 
 class StartupSizeViewSet(GenericViewSet, ListModelMixin):
@@ -22,3 +23,16 @@ class StartupSizeViewSet(GenericViewSet, ListModelMixin):
                 many=True
             ).data
         )
+
+
+class StartupViewSet(ViewSet):
+    def list(self, request):
+        startups = Startup.objects.all()
+        serializer = StartupSerializer(startups, many=True)
+        return Response(serializer.data, status=200)
+    
+    def retrieve(self, request, pk):
+        startups = Startup.objects.all()
+        startup = get_object_or_404(startups, pk)
+        serializer = StartupSerializer(startup)
+        return Response(serializer.data, status=200)
