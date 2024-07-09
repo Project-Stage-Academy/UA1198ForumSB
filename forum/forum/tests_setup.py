a88e9f5c5cbf3c5cd25103d8fbf4acff072e1fb0
m-o-d-e-r <moder.mode.on@gmail.com>
2024-07-09 18:32:02 +0300

    update tests

--- forum/forum/tests_setup.py
+++ forum/forum/tests_setup.py
@@ -10,15 +10,15 @@ class UserSetupMixin(APITestCase):
         self.setup_test_user()
 
     def setup_test_user(self) -> None:
-        self.test_user = CustomUser.objects.create_user(
+        self.test_user: CustomUser = CustomUser.objects.create_user(
             first_name="test_first",
             last_name="test_last",
             email="test@gmail.com",
             password="test_password"
         )
-        self.client.credentials(
-            HTTP_AUTHORIZATION=f'Bearer {AccessToken.for_user(new_user)}'
-        )
+
+        token = AccessToken.for_user(self.test_user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
 
     def tearDown(self) -> None:
         self.client.logout()
--- forum/projects/tests.py
+++ forum/projects/tests.py
@@ -1,7 +1,7 @@
 from rest_framework.reverse import reverse
 from rest_framework import status
-from forum.tests_setup import UserSetupMixin
 
+from forum.tests_setup import UserSetupMixin
 from projects.models import Industry
 
 
@@ -9,18 +9,25 @@ class IndustryModelTestCase(UserSetupMixin):
     CREATE_INDUSTRY_COUNT: int = 3
 
     def setUp(self) -> None:
-        self.setup_test_user()
+        super().setUp()
 
         # create industries
         for i in range(self.CREATE_INDUSTRY_COUNT):
-            tmp_industry = Industry.objects.create(
+            Industry.objects.create(
                 name=f"industry {i}",
                 description=f"test description {i}"
             )
-            Industry.objects.get(industry_id=tmp_industry.industry_id)
 
     def test_projects_industries(self):
         response = self.client.get(reverse('industries-list'))
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.json()), self.CREATE_INDUSTRY_COUNT)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK,
+            "Status code is different from 200"
+        )
+        self.assertEqual(
+            len(response.json()),
+            self.CREATE_INDUSTRY_COUNT,
+            "The number of objects received is not equal to the number created"
+        )
