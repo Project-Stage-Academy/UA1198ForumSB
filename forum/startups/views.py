cda84deffdb17d320978447984364058b8cd6126
Ivblackking <ivan.lysenko305@gmail.com>
2024-07-09 11:37:27 +0300

    added project info to response in retrieve method in StartupViewSet

--- forum/startups/views.py
+++ forum/startups/views.py
@@ -7,6 +7,9 @@ from drf_yasg.utils import swagger_auto_schema
 from .models import StartupSize, Startup
 from .serializers import StartupSizeSerializer, StartupSerializer
 
+from projects.models import Project
+from projects.serializers import ProjectSerializer
+
 
 class StartupSizeViewSet(GenericViewSet, ListModelMixin):
     queryset = StartupSize.objects.all()
@@ -36,5 +39,13 @@ class StartupViewSet(ViewSet):
             startup = Startup.objects.get(startup_id=pk)
         except Startup.DoesNotExist:
             return Response(status=404)
-        serializer = StartupSerializer(startup)
-        return Response(serializer.data, status=200)
+        startup_serializer = StartupSerializer(startup)
+        project = Project.objects.filter(startup=startup).first()
+        if project:
+            project_serializer = ProjectSerializer(project)
+            response_detail = {
+                **startup_serializer.data,
+                "project": project_serializer.data
+            }
+            return Response(response_detail, status=200)
+        return Response(startup_serializer.data, status=200)
