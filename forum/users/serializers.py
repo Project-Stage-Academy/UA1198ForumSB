569e925159f5d750e511e84e22f0161d2d0eee8c
m-o-d-e-r <moder.mode.on@gmail.com>
2024-07-09 11:15:30 +0300

    add password validation

--- forum/users/serializers.py
+++ forum/users/serializers.py
@@ -7,4 +7,7 @@ class PasswordResetRequestSerializer(serializers.Serializer):
 
 class PasswordResetSerializer(serializers.Serializer):
     reset_token = serializers.CharField(max_length=100, required=True)
-    password = serializers.CharField(max_length=255, required=True)
+    password = serializers.RegexField(
+        regex="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$",
+        required=True
+    )
