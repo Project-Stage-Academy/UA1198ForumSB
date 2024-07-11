a28df493dbeef4030863ff044cb8a8173ef4b6ee
Ivblackking <ivan.lysenko305@gmail.com>
2024-07-11 11:32:19 +0300

    moved filtering and searching functions to helpers file

--- forum/startups/views.py
+++ forum/startups/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404
 from rest_framework.mixins import ListModelMixin
 from rest_framework.viewsets import GenericViewSet, ViewSet
 from rest_framework.response import Response
-from django.db.models import Q
 from drf_yasg.utils import swagger_auto_schema
 
 from .models import StartupSize, Startup
@@ -10,6 +9,8 @@ from .serializers import StartupSizeSerializer, StartupSerializer
 
 from projects.models import Project, Industry
 from projects.serializers import ProjectSerializer, IndustrySerializer
+
+from .helpers import select_startups_by_search_string, filter_startups
 
 
 class StartupSizeViewSet(GenericViewSet, ListModelMixin):
@@ -27,49 +28,6 @@ class StartupSizeViewSet(GenericViewSet, ListModelMixin):
                 many=True
             ).data
         )
-
-
-def select_startups_by_search_string(search_string):
-    return Startup.objects.filter(
-        Q(name__icontains = search_string) |
-        Q(location__icontains = search_string) |
-        Q(description__icontains = search_string)
-    )
-
-
-def filter_startups(query_params):
-    industry_name = query_params.get("industry")
-    max_budget = query_params.get("max-budget")
-    size = query_params.get("size")
-    
-    startups = Startup.objects.filter()
-
-    if industry_name:
-        industry = Industry.objects.filter(name=industry_name).first()
-        projects = Project.objects.filter(industries=industry)
-        startups_ids = list(map(lambda proj: proj.startup.startup_id, projects))
-        startups = startups.filter(pk__in=startups_ids)
-    
-    if max_budget:
-        try:
-            max_budget = int(max_budget)
-        except ValueError:
-            return []
-        projects = Project.objects.filter(budget__lte=max_budget)
-        startups_ids = list(map(lambda proj: proj.startup.startup_id, projects))
-        startups = startups.filter(pk__in=startups_ids)
-        
-    if size:
-        try:
-            size = int(size)
-        except ValueError:
-            return []
-        startups = startups.filter(
-            Q(size__people_count_min__lte=size) &
-            Q(size__people_count_max__gt=size)
-        )
-        
-    return startups
 
 
 class StartupViewSet(ViewSet):
