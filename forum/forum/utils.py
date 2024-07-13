70ee7827109fddc6986e5980489e88ff14deb740
m-o-d-e-r <moder.mode.on@gmail.com>
2024-07-13 12:51:52 +0300

    fix type hinting

--- forum/forum/utils.py
+++ forum/forum/utils.py
@@ -5,7 +5,7 @@ from django.template.loader import render_to_string
 
 def build_email_message(
     email_template_path: str,
-    context: dict[str, Any] = None
+    context: dict[str, Any] | None = None
 ) -> str:
     return render_to_string(
         email_template_path,
