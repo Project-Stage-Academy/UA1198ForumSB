cd0c91cf2d8a880e1e01e277f84490ae672cb057
m-o-d-e-r <moder.mode.on@gmail.com>
2024-07-15 17:27:00 +0300

    update `send_notification` function

--- forum/communications/utils.py
+++ forum/communications/utils.py
@@ -11,10 +11,14 @@ def send_message(room_name: str, message: str):
     )
 
 
-def send_notification(user_id: int, message: str | dict):
+def send_notification(receivers: int | list[int], message: str | dict):
     channel_layer = get_channel_layer()
 
-    async_to_sync(channel_layer.group_send)(
-        f"notifications_{user_id}",
-        {"type": "notify_user", "message": message}
-    )
+    if isinstance(receivers, int):
+        receivers = [receivers]
+
+    for user_id in receivers:
+        async_to_sync(channel_layer.group_send)(
+            f"notifications_{user_id}",
+            {"type": "notify_user", "message": message}
+        )
