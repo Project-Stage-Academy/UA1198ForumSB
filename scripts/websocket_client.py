81e3bc1f5689dded236fc1b41b7818c3e284a9e3
m-o-d-e-r <moder.mode.on@gmail.com>
2024-07-15 15:15:18 +0300

    create dummy ws client script

---
+++ scripts/websocket_client.py
@@ -0,0 +1,59 @@
+#! /bin/env python
+
+from websocket import WebSocket
+from os import environ
+import time
+
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+FORUM_HOST = environ.get("FORUM_HOST", "127.0.0.1")
+FORUM_PORT = environ.get("FORUM_PORT", 8000)
+FORUM_USER_EMAIL = environ.get("FORUM_USER_EMAIL")
+FORUM_USER_PASSWORD = environ.get("FORUM_USER_PASSWORD")
+
+
+def get_jwt_access() -> str:
+    response = requests.post(
+        f"http://{FORUM_HOST}:{FORUM_PORT}/users/token/",
+        json={
+            "email": FORUM_USER_EMAIL,
+            "password": FORUM_USER_PASSWORD
+        }
+    )
+    if response.status_code != 200:
+        print("[-] Password auth failed")
+        exit(0)
+
+    access_token = response.json().get("access")
+    if not access_token:
+        print("[-] There is no access token in response JSON")
+        exit(0)
+
+    return access_token
+
+
+def listen_for_notification(ws_client: WebSocket, timeout: float = 0.5):
+    message_num = 1
+    while True:
+        print(f"[{message_num}] {ws_client.recv()}")
+        message_num += 1
+        time.sleep(timeout)
+
+
+if __name__ == "__main__":
+    access_token = get_jwt_access()
+
+    ws_client = WebSocket()
+    ws_client.connect(
+        f"ws://{FORUM_HOST}:{FORUM_PORT}/ws/notifications/",
+        header=[
+            f"Authorization: Bearer {access_token}"
+        ]
+    )
+
+    listen_for_notification(ws_client)
