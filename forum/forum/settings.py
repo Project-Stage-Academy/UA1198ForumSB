855a0e51fcdfc5b3a6b07ede802b695b7bb7f8ce
Iryna Lytvyn <i.litvin27@gmail.com>
2024-07-16 16:03:33 +0300

    Implementing Startup Company Profile Operations

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -202,5 +202,3 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
-
-AUTH_USER_MODEL = 'users.CustomUser'
--- forum/users/urls.py
+++ forum/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework.throttling import ScopedRateThrottle
 
+from startups.views import UserStartupsView, UserStartupView
 from users import views
 
 app_name = 'users'
@@ -10,4 +11,6 @@ urlpatterns = [
          name='token_obtain_pair'),
     path('token/refresh/', views.TokenRefreshView.as_view(throttle_classes=[ScopedRateThrottle]),
          name='token_refresh'),
+    path('<int:user_id>/startups/', UserStartupsView.as_view(), name='user_startups'),
+    path('<int:user_id>/startups/<int:startup_id>/', UserStartupView.as_view(), name='user_startup'),
 ]
