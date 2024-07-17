39b1ac43614a1608594d07a72c6ab4db41687042
Maksym <maksimlevchnko@gmail.com>
2024-07-17 11:01:43 +0300

    moved UserInvestorsView, UserInvestorView to investors app

--- forum/investors/views.py
+++ forum/investors/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_list_or_404, get_object_or_404
 
+from .models import Investor
+from .serializers import InvestorSerializer
+
+from users.permissions import *
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+
+class UserInvestorsView(APIView):
+    permission_classes = [
+        IsAuthenticated,
+        ThisUserPermission,
+        IsInvestorNamespaceSelected
+    ]
+
+    def get(self, request, user_id):
+        investors = get_list_or_404(Investor, user=user_id)
+        serializer = InvestorSerializer(investors, many=True)
+        return Response(serializer.data, status=200)
+
+    def post(self, request, user_id):
+        serializer = InvestorSerializer(data={**request.data, 'user': user_id})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+
+class UserInvestorView(APIView):
+    permission_classes = [
+        IsAuthenticated,
+        ThisUserPermission,
+        IsInvestorNamespaceSelected,
+        ThisInvestor
+    ]
+
+    def get(self, request, user_id, investor_id):
+        investor = get_object_or_404(Investor, user=user_id, investor_id=investor_id)
+        serializer = InvestorSerializer(investor)
+        return Response(serializer.data, status=200)
+
+    def patch(self, request, user_id, investor_id):
+        investor = get_object_or_404(Investor, user=user_id, investor_id=investor_id)
+        serializer = InvestorSerializer(investor, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=200)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, user_id, investor_id):
+        investor = get_object_or_404(Investor, user=user_id, investor_id=investor_id)
+        investor.delete()
+        return Response(status=204)
--- forum/users/urls.py
+++ forum/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework.throttling import ScopedRateThrottle
 
+from investors.views import UserInvestorsView, UserInvestorView
 from startups.views import UserStartupsView, UserStartupView
 from users import views
 
@@ -12,8 +13,8 @@ urlpatterns = [
     path('token/refresh/', views.TokenRefreshView.as_view(throttle_classes=[ScopedRateThrottle]),
          name='token_refresh'),
     path('select-namespace/', views.NamespaceSelectionView.as_view(), name='namespace_selection'),
-    path('<int:user_id>/investors/', views.UserInvestorListView.as_view()),
-    path('<int:user_id>/investors/<int:investor_id>/', views.UserInvestorDetailView.as_view()),
+    path('<int:user_id>/investors/', UserInvestorsView.as_view(), name='user_investors'),
+    path('<int:user_id>/investors/<int:investor_id>/', UserInvestorView.as_view(), name='user_investor'),
     path('register/', views.UserRegisterView.as_view(), name='register'),
     path('email-verify/<str:token>/', views.SendEmailConfirmationView.as_view(), name='email-verify'),
     path('<int:user_id>/startups/', UserStartupsView.as_view(), name='user_startups'),
