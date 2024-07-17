f892bc29ad745ba9b347882de58f597664d5691d
m-o-d-e-r <moder.mode.on@gmail.com>
2024-07-17 11:43:34 +0300

    prepare models for notification system

---
+++ forum/communications/mongo_models.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+from mongoengine import Document, EmbeddedDocument
+from mongoengine import fields
+
+
+class NamespaceInfo(EmbeddedDocument):
+    user_id = fields.LongField()
+    namespace = fields.StringField(required=True)
+    namespace_id = fields.LongField()
+
+
+class Notification(Document):
+    initiator = fields.EmbeddedDocumentField(NamespaceInfo, required=True)
+    receivers = fields.EmbeddedDocumentListField(NamespaceInfo)
+    message = fields.StringField(required=True, max_length=255)
+    created_at = fields.DateTimeField(default=datetime.now)
+
+
+# Notification schema
+# {
+#     "initiator": {
+#         "namespace": "",
+#         "namespace_id": 0
+#     },
+#     "receivers": [
+#         {
+#             "namespace": "",
+#             "namespace_id": 0
+#         },
+#         {
+#             "namespace": "",
+#             "namespace_id": 0
+#         }
+#     ],
+#     "message": "",
+#     "status": "",
+#     "created_at": ""
+# }
--- forum/communications/utils.py
+++ forum/communications/utils.py
@@ -1,5 +1,13 @@
+from abc import ABC, abstractmethod
+
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
+from django.shortcuts import get_object_or_404
+
+from startups.models import Startup
+from projects.models import Project, ProjectSubscription
+from investors.models import Investor
+from .mongo_models import NamespaceInfo, Notification
 
 
 def send_message(room_name: str, message: str):
@@ -11,14 +19,97 @@ def send_message(room_name: str, message: str):
     )
 
 
-def send_notification(receivers: int | list[int], message: str | dict):
+def send_notification(notification: Notification):
     channel_layer = get_channel_layer()
 
-    if isinstance(receivers, int):
-        receivers = [receivers]
-
-    for user_id in receivers:
+    for receiver in notification.receivers:
         async_to_sync(channel_layer.group_send)(
-            f"notifications_{user_id}",
-            {"type": "notify_user", "message": message}
+            f"notifications_{receiver.user_id}",
+            {"type": "notify_user", "message": notification.message}
         )
+
+
+class NotificationManager(ABC):
+    NAMESPACE_NAME: str = None
+    NAMESPACE_RECEIVERS_NAME: str = None
+
+    def __init__(self, namespace_obj: Investor | Startup) -> None:
+        self.namespace = namespace_obj
+
+    def _create_initiator_namespace(self) -> NamespaceInfo:
+        return NamespaceInfo(
+            user_id=self.namespace.user.user_id,
+            namespace=self.NAMESPACE_NAME,
+            namespace_id=self.get_namespace_id()
+        )
+
+    @abstractmethod
+    def _create_receivers_namespaces(self) -> list[NamespaceInfo]:
+        ...
+
+    @abstractmethod
+    def get_namespace_id(self) -> int:
+        ...
+
+    def push_notification(self, message: str):
+        initiator_namespace = self._create_initiator_namespace()
+        receivers_namespaces = self._create_receivers_namespaces()
+
+        notification = Notification(
+            initiator=initiator_namespace,
+            receivers=receivers_namespaces,
+            message=message
+        )
+        notification.save()
+
+        send_notification(notification)
+
+
+class StartupNotificationManager(NotificationManager):
+    NAMESPACE_NAME = "startup"
+    NAMESPACE_RECEIVERS_NAME = "investor"
+
+    def _create_receivers_namespaces(self) -> list[NamespaceInfo]:
+        receivers: list[NamespaceInfo] = []
+
+        project: Project = get_object_or_404(
+            Project,
+            startup_id=self.namespace.startup_id
+        )
+        for i in ProjectSubscription.objects.filter(project=project):
+            receivers.append(
+                NamespaceInfo(
+                    user_id=i.investor.user.user_id,
+                    namespace=self.NAMESPACE_RECEIVERS_NAME,
+                    namespace_id=i.investor.investor_id
+                )
+            )
+            i.investor.investor_id
+
+        return receivers
+
+    def get_namespace_id(self) -> int:
+        return self.namespace.startup_id
+
+
+class InvestorNotificationManager(NotificationManager):
+    NAMESPACE_NAME = "investor"
+    NAMESPACE_RECEIVERS_NAME = "startup"
+
+    def _create_receivers_namespaces(self) -> list[NamespaceInfo]:
+        receivers: list[NamespaceInfo] = []
+
+        for i in ProjectSubscription.objects.filter(investor=self.namespace):
+            receivers.append(
+                NamespaceInfo(
+                    user_id=i.investor.user.user_id,
+                    namespace=self.NAMESPACE_RECEIVERS_NAME,
+                    namespace_id=i.project.startup.startup_id
+                )
+            )
+            i.investor.investor_id
+
+        return receivers
+
+    def get_namespace_id(self) -> int:
+        return self.namespace.investor_id
