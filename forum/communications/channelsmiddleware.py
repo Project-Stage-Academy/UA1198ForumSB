a1bb8f285812e22e699f92b147ed8592831f87e1
m-o-d-e-r <moder.mode.on@gmail.com>
2024-07-18 17:20:53 +0300

    fix var naming

--- forum/communications/channelsmiddleware.py
+++ forum/communications/channelsmiddleware.py
@@ -1,14 +1,14 @@
 from channels.db import database_sync_to_async
 from channels.middleware import BaseMiddleware
-from rest_framework.exceptions import APIException
-from rest_framework_simplejwt.exceptions import AuthenticationFailed
+from rest_framework_simplejwt.exceptions import (
+    AuthenticationFailed,
+    InvalidToken,
+    TokenError,
+)
 from rest_framework_simplejwt.tokens import AccessToken
-from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
-
 from users.models import CustomUser
 
-
-AUTH_HEAR_KEY = b'authorization'
+AUTH_HEADER_KEY = b'authorization'
 
 
 @database_sync_to_async
@@ -29,11 +29,11 @@ class JwtAuthMiddleware(BaseMiddleware):
     async def __call__(self, scope, receive, send):
         headers = dict(scope["headers"])
 
-        if AUTH_HEAR_KEY not in headers:
+        if AUTH_HEADER_KEY not in headers:
             raise AuthenticationFailed("Auth header was not provided")
 
         try:
-            _, token_key = headers[AUTH_HEAR_KEY].decode().split()
+            _, token_key = headers[AUTH_HEADER_KEY].decode().split()
             jwt_payload: dict = AccessToken(token_key).payload
 
             scope["user"] = await get_user(jwt_payload["user_id"])
