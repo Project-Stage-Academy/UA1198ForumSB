040849dabffa3ab142d274d0c4571aa8e45839ab
m-o-d-e-r <moder.mode.on@gmail.com>
2024-07-25 22:11:56 +0300

    redesign utils.py

--- forum/communications/utils.py
+++ forum/communications/utils.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Any
 
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
@@ -8,6 +9,7 @@ from projects.models import Project, ProjectSubscription
 from rest_framework.serializers import Serializer
 from startups.models import Startup
 
+from .exceptions import BaseNotificationException, InvalidDataError, MessageTypeError
 from .mongo_models import NamespaceInfo, Notification
 from .serializers import (
     ChatMessageSerializer,
@@ -17,144 +19,118 @@ from .serializers import (
     WSServerMessageSerializer,
 )
 
-MESSAGE_TYPES: dict[str, Serializer] = {
-    "chat_message": ChatMessageSerializer,            # used in chat implementation
-    "notify_user": WSNotificationSerializer,          # for notifications
-    "notification_ack": WSNotificationAckSerializer,  # for notification acknowledge
-    "server_error": WSServerMessageSerializer,        # server side error (connection will be closed)
-    "client_error": WSClientMessageSerializer         # client side error (connection will be closed)
-}
 
+class BaseWSMessageBuilder(ABC):
+    def __init__(self) -> None:
+        self.ws_message: dict[str, Any] = None
 
-async def send_raw_ws_message(room_name: str, raw_message: dict) -> None:
-    channel_layer = get_channel_layer()
-
-    try:
-        await channel_layer.group_send(room_name, raw_message)
-    except ValueError:
-        # TODO: call logging function
-        # no handler for this message type
+    @abstractmethod
+    def build(self, *args, **kwargs) -> None:
         ...
 
+    async def send(self, room_name: str):
+        channel_layer = get_channel_layer()
 
-def _is_valid_message_type(message_type: str) -> bool:
-    return bool(MESSAGE_TYPES.get(message_type))
-
-
-async def _get_serializer(
-    message_type: str,
-    room_name: str,
-    *,
-    by_client: bool = True
-) -> Serializer:
-    if not _is_valid_message_type(message_type):
-        if by_client:
-            await send_raw_ws_message(
-                room_name,
-                {
-                    "type": "client_error",
-                    "message": "Invalid field 'type' provided"
-                }
-            )
-            return
-        else:
+        try:
+            await channel_layer.group_send(room_name, self.ws_message)
+        except ValueError:
             # TODO: call logging function
-            await send_raw_ws_message(
-                room_name,
-                {
-                    "type": "server_error",
-                    "message": "Something gone wrong, please check if data valid"
-                }
-            )
-            return
-
-    return MESSAGE_TYPES[message_type]
+            # no handler for this ws_message type
+            ...
 
 
-async def apply_serializer(
-    raw_message: dict,
-    room_name: str,
-    *,
-    by_client: bool = True
-) -> dict:
-    """
-        Automatically determine which serializer should be used to validate data.
-        Return validated data.
+class NotificationBuilder(BaseWSMessageBuilder):
+    def build(self, notification: Notification) -> None:
+        self.ws_message = {
+            "type": "notify_user",
+            "notification_id": str(notification.pk),
+            "initiator": notification.initiator.to_mongo().to_dict(),
+            "message": notification.message,
+            "created_at": str(notification.created_at)
+        }
 
-    Args:
-        raw_message (dict): websocket message
-        room_name (str): _description_
-        by_client (bool, optional): Side where websocket message built. Defaults to True.
-        If the message is built by the client and any error occurs client is responsible for it.
 
-    Returns:
-        dict: Validated data
-    """
+class ServerErrorBuilder(BaseWSMessageBuilder):
+    def build(self, message: str) -> None:
+        self.ws_message = {
+            "type": "server_error",
+            "message": message,
+        }
 
-    if not raw_message.get("type"):
-        if by_client:
-            await send_raw_ws_message(
-                room_name,
-                {
-                    "type": "client_error",
-                    "message": "'type' field was not provided"
-                }
-            )
-            return
-        else:
+
+class ClientErrorBuilder(BaseWSMessageBuilder):
+    def build(self, message: str) -> None:
+        self.ws_message = {
+            "type": "client_error",
+            "message": message,
+        }
+
+
+class AutoSerializer:
+    MESSAGE_TYPES: dict[str, Serializer] = {
+        "chat_message": ChatMessageSerializer,            # used in chat implementation
+        "notify_user": WSNotificationSerializer,          # for notifications
+        "notification_ack": WSNotificationAckSerializer,  # for notification acknowledge
+        "server_error": WSServerMessageSerializer,        # server side error (connection will be closed)
+        "client_error": WSClientMessageSerializer         # client side error (connection will be closed)
+    }
+
+    def __init__(self, raw_message: dict, room_name: str) -> None:
+        self.raw_message = raw_message
+        self.room_name = room_name
+
+    def _is_valid_message_type(self) -> bool:
+        return bool(self.MESSAGE_TYPES.get(self.raw_message["type"]))
+
+    async def _get_serializer(self) -> Serializer:
+        if not self.raw_message.get("type"):
+            raise MessageTypeError()
+
+        if not self._is_valid_message_type(self.raw_message["type"]):
+            raise MessageTypeError()
+
+        return self.MESSAGE_TYPES[self.raw_message["type"]]
+
+    async def apply_serializer(self) -> dict:
+        """
+            Automatically determine which serializer should be used to validate data.
+            Return validated data.
+
+        Args:
+            raw_message (dict): websocket message
+            room_name (str): name of the room to send the message to
+            by_client (bool, optional): Side where websocket message built. Defaults to True.
+            If the message is built by the client and any error occurs client is responsible for it.
+
+        Returns:
+            dict: Validated data
+        """
+
+        serializer_class: Serializer = await self._get_serializer()
+        serializer: Serializer = serializer_class(data=self.raw_message)
+        if not serializer.is_valid():
+            raise InvalidDataError()
+
+        return serializer.validated_data
+
+    async def apply_for_client_message(self) -> dict:
+        try:
+            validated_data = await self.apply_serializer()
+            return validated_data
+        except BaseNotificationException as exc:
+            client_error_builder = ServerErrorBuilder()
+            client_error_builder.build(exc.message)
+            await client_error_builder.send(self.room_name)
+
+    async def apply_for_server_message(self) -> dict:
+        try:
+            validated_data = await self.apply_serializer()
+            return validated_data
+        except BaseNotificationException as exc:
             # TODO: call logging function
-            await send_raw_ws_message(
-                room_name,
-                {
-                    "type": "server_error",
-                    "message": "Something gone wrong, please check if data valid"
-                }
-            )
-            return
-
-    serializer_class: Serializer = await _get_serializer(
-        raw_message["type"],
-        room_name
-    )
-    serializer: Serializer = serializer_class(data=raw_message)
-    if not serializer.is_valid():
-        if by_client:
-            await send_raw_ws_message(
-                room_name,
-                {
-                    "type": "client_error",
-                    "message": "Invalid data provided"
-                }
-            )
-            return
-        else:
-            # TODO: call logging function
-            await send_raw_ws_message(
-                room_name,
-                {
-                    "type": "server_error",
-                    "message": "Something gone wrong, please check if data valid"
-                }
-            )
-            return
-
-    return serializer.validated_data
-
-
-def send_notification(notification: Notification):
-    channel_layer = get_channel_layer()
-
-    for receiver in notification.receivers:
-        async_to_sync(channel_layer.group_send)(
-            f"notifications_{receiver.user_id}",
-            {
-                "type": "notify_user",
-                "notification_id": str(notification.pk),
-                "initiator": notification.initiator.to_mongo().to_dict(),
-                "message": notification.message,
-                "created_at": str(notification.created_at)
-            }
-        )
+            server_error_builder = ServerErrorBuilder()
+            server_error_builder.build(exc.message)
+            await server_error_builder.send(self.room_name)
 
 
 class NotificationManager(ABC):
@@ -190,7 +166,11 @@ class NotificationManager(ABC):
         )
         notification.save()
 
-        send_notification(notification)
+        notification_builder = NotificationBuilder()
+        notification_builder.build(notification)
+
+        for receiver in notification.receivers:
+            async_to_sync(notification_builder.send)(f"notifications_{receiver.user_id}")
 
 
 class StartupNotificationManager(NotificationManager):
