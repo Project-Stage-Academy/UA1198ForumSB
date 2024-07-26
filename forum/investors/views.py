4ef28ba1666b1db895ff51a36a99751b5d36caac
IrinLytvyn <150450436+IrinLytvyn@users.noreply.github.com>
2024-07-26 16:08:34 +0300

    55 api endpoint to list and manage saved startups 24 3 (#76)
    * add endpoint for listing saved startups
    
    * add endpoint to unfollow a startup
    
    * fixed typo
    
    * added permissions and swagger
    
    * deleted import

--- forum/investors/views.py
+++ forum/investors/views.py
@@ -1,14 +1,18 @@
+import json
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_list_or_404, get_object_or_404
+from drf_yasg.utils import swagger_auto_schema
 
-from .models import Investor
+from startups.models import Startup
+from startups.serializers import StartupSerializer
+from .models import Investor, InvestorStartup
 from .serializers import InvestorSerializer, InvestorSaveStartupSerializer
 
 from users.permissions import *
 from rest_framework.permissions import IsAuthenticated
-from .permissions import InvestorSaveStartupPermission
+from .permissions import InvestorPermission, ThisInvestorPermission
 
 
 INVESTOR_BASE_PERMISSIONS = [
@@ -62,9 +66,9 @@ class UserInvestorView(APIView):
 class InvestorSaveStartupView(APIView):
     permission_classes = [
         IsAuthenticated,
-        InvestorSaveStartupPermission
+        InvestorPermission
     ]
-    
+
     def post(self, request, startup_id):
         payload = get_token_payload_from_cookies(request)
         investor_id = payload.get("name_space_id")
@@ -78,3 +82,75 @@ class InvestorSaveStartupView(APIView):
             return Response("The startup has been successfully saved.",
                             status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class InvestorSavedStartupsView(APIView):
+    permission_classes = [
+        IsAuthenticated,
+        ThisInvestorPermission
+    ]
+
+    @swagger_auto_schema(
+        operation_description="List of startups saved by the investor",
+        operation_summary="Get list of startups",
+        responses={
+            status.HTTP_200_OK: StartupSerializer(),
+            status.HTTP_400_BAD_REQUEST: 'BadRequest'
+        }
+    )
+    def get(self, request, user_id, investor_id):
+        order_by = request.query_params.get('order_by')
+        filter_ = request.query_params.get('filter')
+
+        if filter_:
+            try:
+                filter_data = json.loads(filter_)
+            except json.JSONDecodeError:
+                return Response({'error': 'Invalid filter parameter'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            filter_data = {}
+
+        startups = Startup.objects.filter(
+            is_deleted=False,
+            interested_investors__investor__investor_id=investor_id,
+            interested_investors__investor__user_id=user_id
+        )
+
+        for key, value in filter_data.items():
+            if key == 'size':
+                lookup = key
+            else:
+                lookup = f'{key}__icontains'
+            startups = startups.filter(**{lookup: value})
+
+        if order_by:
+            startups = startups.order_by(order_by)
+
+        serializer = StartupSerializer(startups, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UnsaveStartupView(APIView):
+    permission_classes = [
+        IsAuthenticated,
+        InvestorPermission
+    ]
+
+    @swagger_auto_schema(
+        operation_description="Unfollow a startup by the investor",
+        operation_summary="unfollow a startup",
+        responses={
+            status.HTTP_204_NO_CONTENT: None,
+            status.HTTP_404_NOT_FOUND: 'NotFound'
+        }
+    )
+    def delete(self, request, startup_id):
+        payload = get_token_payload_from_cookies(request)
+        investor_id = payload.get("name_space_id")
+        try:
+            startup = InvestorStartup.objects.get(startup_id=startup_id, investor_id=investor_id)
+            startup.delete()
+        except InvestorStartup.DoesNotExist:
+            return Response({'error': 'Subscription is not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
