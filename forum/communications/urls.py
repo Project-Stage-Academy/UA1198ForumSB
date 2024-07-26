59dd2cac53fa6b1bd1b480c61fc08dd31af5eb2e
Ivblackking <ivan.lysenko305@gmail.com>
2024-07-26 13:43:47 +0300

    added urls for comunications

--- forum/communications/urls.py
+++ forum/communications/urls.py
@@ -1,2 +1,11 @@
+from django.urls import path
+import views
 
-urlpatterns = []
+
+urlpatterns = [
+    path("conversations/create", views.CreateConversationView.as_view(),
+         name="create_conversation"),
+    path("messages/send", views.SendMessageView.as_view(), name="send_message"),
+    path("conversations/<int:conversation_id>/messages", views.MessagesListView.as_view(),
+         name="conversation_messages")
+]
