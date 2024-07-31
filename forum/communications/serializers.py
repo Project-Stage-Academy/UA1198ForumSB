3cb9f9295d1471c82692e487e7f98697a2245e2d
Ivblackking <ivan.lysenko305@gmail.com>
2024-07-31 12:40:53 +0300

    added first version of views and serializers

--- forum/communications/serializers.py
+++ forum/communications/serializers.py
@@ -1,10 +1,51 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
+
+from users.models import CustomUser
+from investors.models import Investor
+from startups.models import Startup
+from .mongo_models import Room
+
+
+class RoomSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=128, unique=True, required=True)
+    participants_id = serializers.ListField(
+        child=serializers.IntegerField(required=True)
+    )
+    created_at = serializers.DateTimeField()
+    updated_at = serializers.DateTimeField()
+
+    def validate(self, data):
+        participants_id = data.get("participants_id")
+
+        for id in participants_id:
+            get_object_or_404(CustomUser, user_id=id)
+    
+        return data
 
 
 class ChatMessageSerializer(serializers.Serializer):
-    # TODO: implement this serializer in an appropriate task
-    ...
+    room = serializers.CharField(required=True)
+    namespace_id = serializers.IntegerField(required=True)
+    namespace_name = serializers.CharField(required=True)
+    content = serializers.CharField(required=True)
+    timestamp = serializers.DateTimeField()
 
+    def validate(self, data):
+        room_id = str(data.get("room").id)
+        namespace_id = data.get("namespace_id")
+        namespace_name = data.get("namespace_name")
+        
+        get_object_or_404(Room, id=room_id)
+
+        if namespace_name == "startup":
+            get_object_or_404(Startup, startup_id=namespace_id)
+        elif namespace_name == "investor":
+            get_object_or_404(Investor, startup_id=namespace_id)
+        else:
+            raise serializers.ValidationError("Invalid namespace")
+        
+        return data
 
 class BaseWSMessageSerializer(serializers.Serializer):
     type = serializers.CharField(required=True)
--- forum/communications/views.py
+++ forum/communications/views.py
@@ -1,16 +1,33 @@
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .mongo_models import Room, Message
+from .serializers import RoomSerializer, ChatMessageSerializer
 
 
 class CreateConversationView(APIView):
     def post(request):
-        pass
+        serializer = RoomSerializer(data=request.data)
+        if serializer.is_valid():
+            new_room = Room(**serializer.data)
+            new_room.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class SendMessageView(APIView):
     def post(request):
-        pass
+        serializer = ChatMessageSerializer(data=request.data)
+        if serializer.is_valid():
+            new_message = Message(**serializer.data)
+            new_message.save()
+            # TODO call NotificationManager to send new_message.id
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class MessagesListView(APIView):
     def get(request, conversation_id):
-        pass
+        messages = Message.objects.filter(room__id = conversation_id)
+        serializer = ChatMessageSerializer(messages, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
