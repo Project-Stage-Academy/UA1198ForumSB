6b2dad0c1c51bd8708567b4eb453680ed931c3e4
Maryana Ryvak <ryvak.maryana@gmail.com>
2024-08-01 10:42:05 +0300

    define model for notification preferences

--- forum/communications/mongo_models.py
+++ forum/communications/mongo_models.py
@@ -27,7 +27,34 @@ class Notification(BaseTimestampModel):
     initiator = fields.EmbeddedDocumentField(NamespaceInfo, required=True)
     receivers = fields.EmbeddedDocumentListField(NamespaceInfo, required=True)
     message = fields.StringField(required=True, max_length=255)
-    
+
+
+class NotificationTypes(Document):
+    name = fields.StringField(required=True, unique=True, max_length=50)
+    description = fields.StringField(max_length=255)
+
+
+class NotificationPreferences(Document):
+    user_id = fields.LongField(required=True)
+    notification_types = fields.EmbeddedDocumentListField(NotificationTypes)
+
+    ws_enabled = fields.BooleanField(default=True)
+    email_enabled = fields.BooleanField(default=True)
+
+    @classmethod
+    def is_ws_enabled(cls, user_id, notification_type_name):
+        preference = cls.objects(user_id=user_id, notification_types_name=notification_type_name).first()
+        return preference.ws_enabled if preference else False
+
+    @classmethod
+    def is_email_enabled(cls, user_id, notification_type_name):
+        preference = cls.objects(user_id=user_id, notification_types_name=notification_type_name).first()
+        return preference.email_enabled if preference else False
+
+    @classmethod
+    def has_preferences(cls, user_id, notification_type_name):
+        return cls.objects(user_id=user_id, notification_types__name=notification_type_name).first() is not None
+
 
 class Room(BaseTimestampModel):
     name = fields.StringField(max_length=128, unique=True, required=True)
