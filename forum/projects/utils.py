1f19183de81a88e76344edfb7ae7b0d5794c1ef5
Nazar Humeniuk <nazarhumeniuk@gmail.com>
2024-08-08 09:16:09 +0200

    add instance verification

--- forum/projects/utils.py
+++ forum/projects/utils.py
@@ -1,7 +1,15 @@
+from projects.models import Project
+from django.core.exceptions import ValidationError
+
+
 def get_changed_fields(old_instance, new_instance):
     """
     Function compare project data and return changed
     """
+
+    if not (isinstance(old_instance, Project) and isinstance(old_instance, Project)):
+        raise ValidationError("Object must be an instance of Project")
+
     changes = {}
     for field in old_instance._meta.fields:
         field_name = field.name
