fe286c4668b329a85d0050e9699715af8a7182e0
Maksym <maksimlevchnko@gmail.com>
2024-08-09 14:23:57 +0300

    implement select namespace

--- forum_frontend/src/App.js
+++ forum_frontend/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import Home from "./components/Home/Home";
 import Login from "./components/Authorization/Login";
 import StartupsList from "./components/StartupsList/StartupsList";
 import RoomsList from "./components/RoomsList/RoomsList";
+import UserNamespacesList from "./components/UserNamespacesList/UserNamespacesList";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="login" element={<Login />} />
           <Route path="startups" element={<StartupsList />} />
           <Route path="inbox" element={<RoomsList/>} />
+          <Route path="my-cabinets" element={<UserNamespacesList/>} />
         </Route>
       </Routes>
     </>
